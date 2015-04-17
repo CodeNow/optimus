82ce04d5ec16377d152bc167cd2cf6a8c7d07aa1
Ryan Sandor Richards <ryan@runnable.com>
2015-04-17 13:37:37 -0700

    Setup index.js, configs, and started the http server.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-throw new Exception('Not Implemented');
+'use strict';
+
+require('loadenv')('optimus:env');
+var ClusterManager = require('cluster-man');
+var monitor = require('monitor-dog');
+var app = require('./lib/app');
+
+/**
+ * User repository transformer.
+ * @module optimus
+ * @author Ryan Sandor Richards
+ */
+
+/**
+ * Master process.
+ */
+function master() {
+  monitor.histogram('status', 1);
+}
+
+/**
+ * Worker process.
+ */
+function worker() {
+  app.listen(process.env.PORT);
+}
+
+/**
+ * Performs additional actions before exiting the master process.
+ * @param {Error} [err] Optional error.
+ * @param {function} done Called when we are done.
+ */
+function beforeExit(err, done) {
+  monitor.histogram('status', 0);
+  done();
+}
+
+// Start the cluster
+new ClusterManager({
+  master: master,
+  worker: worker,
+  beforeExit: beforeExit
+}).start();
