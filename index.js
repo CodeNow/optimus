1e68e369c1b0e5f8be28f995b5b07928b4d2494f
Ryan Sandor Richards <ryan@runnable.com>
2016-03-12 16:12:09 -0800

    Fixing startup

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const app = require('./lib/app')
 const cache = require('./lib/cache')
 const cat = require('error-cat')
 const monitor = require('monitor-dog')
+const log = require('./lib/logger').child({ module: 'index' })
 
 cache.initialize()
   .then(app.start)
   .then(() => { monitor.histogram('status', 1) })
   .catch(cat.catch)
-  .catch(() => {
+  .catch((err) => {
+    log.fatal({ err: err }, 'Optimus failed to start')
     monitor.histogram('status', 0)
     process.exit(1)
   })
